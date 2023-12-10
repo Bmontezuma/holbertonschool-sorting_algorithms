4ded47db31062c8354f3feae1f685e3a2a66b269
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2023-12-10 04:44:48 -0800

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,37 +2,33 @@
 
 /**
  * bubble_sort - Sorts an array of integers in ascending order
- *               using the Bubble Sort algorithm.
- *
+ * using the Bubble sort algorithm.
  * @array: The array to be sorted
  * @size: Number of elements in @array
  */
 void bubble_sort(int *array, size_t size)
 {
-        size_t i, j;
-        int temp;
-        int swapped;
+	size_t i, j;
+	int temp;
 
-        if (!array || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-        for (i = 0; i < size - 1; i++)
-        {
-        swapped = 0;
-        for (j = 0; j < size - i - 1; j++)
-        {
-        if (array[j] > array[j + 1])
-        {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                swapped = 1;
+	for (i = 0; i < size - 1; i++)
+	{
+	for (j = 0; j < size - i - 1; j++)
+	{
+	if (array[j] > array[j + 1])
+	{
+		/* Swap elements */
+		temp = array[j];
+		array[j] = array[j + 1];
+		array[j + 1] = temp;
 
-                print_array(array, size);
-        }
-        }
-
-        if (!swapped)
-            break;
-        }
+		/* Print array after each swap */
+		print_array(array, size);
+		}
+	}
+	}
 }
+
--- sort.h
+++ sort.h
@@ -6,9 +6,9 @@
 /**
  * struct listint_s - Doubly linked list node
  *
- * @n: Integer in the node
- * @prev: Pointer to the previous element of list
- * @next: Pointer to the next element of list
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
  */
 typedef struct listint_s
 {
@@ -21,6 +21,6 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
-void insertion_sort_list(listint_t **list)
-void selection_sort(int *array, size_t size);
+
 #endif /* SORT_H */
+
