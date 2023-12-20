8bfc573b72a6d6e918e7ee670ccf0759bb17d511
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2023-12-19 18:16:27 -0800

    Updated Files

--- sort.h
+++ sort.h
@@ -2,7 +2,6 @@
 #define SORT_H
 
 #include <stddef.h>
-#include <stdio.h>
 
 /* Structure for doubly linked list node */
 typedef struct listint_s
@@ -12,16 +11,23 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+/* Data structure declaration with brief description */
+struct listint_s
+{
+    int n; /* Integer value */
+    struct listint_s *next; /* Pointer to the next node */
+};
 
 /* Function prototypes */
 void print_array(const int *array, size_t size);
 void swap(int *a, int *b);
-int lomuto_partition(int *array, int low, int high);
 void quick_sort_recursive(int *array, int low, int high);
 void quick_sort(int *array, size_t size);
-int lomuto_partition(int *array, int low, int high);
 void insertion_sort_list(listint_t **list);
 void print_list(const listint_t *list);
 void exchange_elements(int *a, int *b);
+size_t lomuto_partition(int *array, int low, int high, size_t size);
+void quicksort(int *array, int low, int high, size_t size);
+void quick_sort(int *array, size_t size);
 #endif /* SORT_H */
 
