43cb12e8b7c332d41cab2b0ebd01fa6971e9ee9f
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2023-12-20 02:07:41 -0800

    Bubbule Sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,30 +1,51 @@
-#include "sort.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include "sort.h"
+
 /**
- * bubble_sort - Sorts array of integers in ascending order.
- * @array: The array to be sorted
+ * swap - Swaps two integers in an array
+ * @a: Pointer to the first integer
+ * @b: Pointer to the second integer
+ */
+void swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * bubble_sort - Sorts an array of integers in ascending order using
+ *               the Bubble Sort algorithm
+ * @array: Array to be sorted
  * @size: Number of elements in the array
  */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int temp;
+	int swapped;
 
 	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-	for (j = 0; j < size - i - 1; j++)
-	{
-		if (array[j] > array[j + 1])
-	{
-		temp = array[j];
-		array[j] = array[j + 1];
-	array[j + 1] = temp;
-	print_array(array, size);
-	}
-	}
+		swapped = 0;
+
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+				swapped = 1;
+			}
+		}
+
+		/* If no two elements were swapped by inner loop, the array is sorted */
+		if (swapped == 0)
+			break;
 	}
 }
+
