8a566db2a21c6de5aa7181f07b0a2e6e9b7668df
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2023-12-20 17:41:58 -0800

    Selection Sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,32 +8,29 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    if (!array || size < 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-    size_t i, j, min_index;
-    int temp;
+	size_t i, j, min_index;
+	int temp;
 
-    for (i = 0; i < size - 1; ++i)
-    {
-        min_index = i;
+	for (i = 0; i < size - 1; ++i)
+	{
+		min_index = i;
 
-        for (j = i + 1; j < size; ++j)
-        {
-            if (array[j] < array[min_index])
-                min_index = j;
-        }
+		for (j = i + 1; j < size; ++j)
+		{
+			if (array[j] < array[min_index])
+				min_index = j;
+		}
 
-        if (min_index != i)
-        {
-            /* Swap elements */
-            temp = array[i];
-            array[i] = array[min_index];
-            array[min_index] = temp;
-
-            /* Print the array after each swap */
-            print_array(array, size);
-        }
-    }
+		if (min_index != i)
+		{
+			temp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = temp;
+			print_array(array, size);
+		}
+	}
 }
 
